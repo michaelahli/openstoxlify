d8e39c5b8108306a43b99cd280abf2a0e86c9343
Michael <michael.ahli99@gmail.com>
2025-05-10 18:14:48 +0700

    rainbow chart example

--- examples/bollinger_bands.py
+++ examples/bollinger_bands.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-from statistics import mean, stdev
-
+import statistics
 from openstoxlify.models import MarketData, Period, PlotType, ActionType, Provider
 from openstoxlify.plotter import plot
 from openstoxlify.fetch import fetch
@@ -13,52 +12,57 @@ def fetch_market_data(symbol: str) -> MarketData:
 
 
 def calculate_bollinger_bands(market_data: MarketData, window: int = 20):
-    quotes = market_data.quotes
-    bands = []
+    prices = [quote.close for quote in market_data.quotes]
+    timestamps = [quote.timestamp for quote in market_data.quotes]
 
-    for i in range(len(quotes) - window + 1):
-        window_quotes = quotes[i : i + window]
-        closes = [q.close for q in window_quotes]
-        sma = mean(closes)
-        sd = stdev(closes)
+    middle = []
+    upper = []
+    lower = []
 
-        timestamp = window_quotes[-1].timestamp
-        upper = sma + 2 * sd
-        lower = sma - 2 * sd
+    for i in range(len(prices) - window + 1):
+        window_prices = prices[i : i + window]
+        sma = sum(window_prices) / window
+        std = statistics.stdev(window_prices)
 
-        bands.append((timestamp, sma, upper, lower))
+        timestamp = timestamps[i + window - 1]
+        middle.append((timestamp, sma))
+        upper.append((timestamp, sma + 2 * std))
+        lower.append((timestamp, sma - 2 * std))
 
-    return bands
-
-
-def plot_bollinger_bands(bands: list[tuple[datetime, float, float, float]]):
-    for timestamp, sma, upper, lower in bands:
-        plot(PlotType.LINE, "SMA", timestamp, sma)
-        plot(PlotType.AREA, "BB Upper", timestamp, upper)
-        plot(PlotType.AREA, "BB Lower", timestamp, lower)
+    return middle, upper, lower
 
 
-def generate_bb_strategy_signals(market_data: MarketData, bands):
-    price_map = {q.timestamp: q.close for q in market_data.quotes}
+def plot_bands(middle, upper, lower):
+    for timestamp, value in middle:
+        plot(PlotType.LINE, "BB Middle", timestamp, value)
+    for timestamp, value in upper:
+        plot(PlotType.LINE, "BB Upper", timestamp, value)
+    for timestamp, value in lower:
+        plot(PlotType.LINE, "BB Lower", timestamp, value)
+
+
+def generate_signals(market_data, middle, upper, lower):
+    band_dict = {
+        ts: (mid, up, low) for (ts, mid), (_, up), (_, low) in zip(middle, upper, lower)
+    }
     last_action = ActionType.HOLD
 
-    for timestamp, sma, upper, lower in bands:
-        price = price_map.get(timestamp)
-        if price is None:
+    for quote in market_data.quotes[19:]:
+        if quote.timestamp not in band_dict:
             continue
 
-        if price > upper and last_action != ActionType.SHORT:
-            act(ActionType.SHORT, timestamp, 1000)
-            last_action = ActionType.SHORT
-        elif price < lower and last_action != ActionType.LONG:
-            act(ActionType.LONG, timestamp, 1000)
+        mid, up, low = band_dict[quote.timestamp]
+
+        if quote.close <= low and last_action != ActionType.LONG:
+            act(ActionType.LONG, quote.timestamp, 1)
             last_action = ActionType.LONG
+        elif quote.close >= up and last_action != ActionType.SHORT:
+            act(ActionType.SHORT, quote.timestamp, 1)
+            last_action = ActionType.SHORT
 
 
 market_data = fetch_market_data("BTC-USD")
-bands = calculate_bollinger_bands(market_data)
-
-plot_bollinger_bands(bands)
-generate_bb_strategy_signals(market_data, bands)
-
+middle_band, upper_band, lower_band = calculate_bollinger_bands(market_data)
+plot_bands(middle_band, upper_band, lower_band)
+generate_signals(market_data, middle_band, upper_band, lower_band)
 draw()
