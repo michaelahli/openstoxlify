6c33262bfb9a453dc5c8bad90c6754441d2183aa
Michael <michael.ahli99@gmail.com>
2026-01-07 22:44:50 +0700

    build on release
    trade file are not tracked on openstoxlify library

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name="openstoxlify", packages=find_packages())
+setup(
+    packages=find_packages(include=["openstoxlify", "openstoxlify.*"]),
+    include_package_data=True,
+)
